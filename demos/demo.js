f4a2b56822ebf32057ca1ab2852003a3204e0d05
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2021-04-07 10:32:20 -0700

    First Assignment Uploaf
    This is the first upload of my index.js and package.json for Assignment 1

---
+++ demos/demo.js
@@ -0,0 +1,21 @@
+console.log('Hello!');
+const http = require('http');
+http.createServer((req,res) => {
+
+    var path = req.url.toLowerCase();
+    switch(path) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('This is the Home Page!');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('This is the About Page! My name is Arthur Tabadero and I am a student for IT122: Javascript 2. I am taking this class to learn more about JS and to learn the ways of Node.js!');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Page not found!');
+            break;
+    }
+
+}).listen(process.env.PORT || 3000);
