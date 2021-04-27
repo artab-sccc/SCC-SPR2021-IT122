e88421cca2b17eefbd25deb040cb3baef65a16d0
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2021-04-26 19:46:06 -0700

    Assignment 2 and 3 re-commited
    Recommitting assignment 2 and 3 as I have been submitting the assignment incorrectly.

--- demos/demo.js
+++ demos/demo.js
@@ -1,16 +1,26 @@
-console.log('Hello!');
-const http = require('http');
+import { read } from 'fs';
+import http from 'http';
+import {getAll, getItem, pokemons} from './data.js';
+import qs from 'querystring';
+
 http.createServer((req,res) => {
 
-    var path = req.url.toLowerCase();
+    let url = req.url.split("?");
+    let path = url[0].toLowerCase();
+    let query = qs.parse(url[1]);
+
     switch(path) {
         case '/':
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('This is the Home Page!');
+            res.end(JSON.stringify(pokemons));
             break;
         case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('This is the About Page! My name is Arthur Tabadero and I am a student for IT122: Javascript 2. I am taking this class to learn more about JS and to learn the ways of Node.js!');
+            break;
+        case '/details':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(JSON.stringify(getItem(query.name)));
             break;
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-console.log('Hello!');
-const http = require('http');
-http.createServer((req,res) => {
+import { read } from 'fs';
+import http from 'http';
+import * as pokemon from './data.js';
+import qs from 'querystring';
+import express from 'express';
+import handlebars from 'express-handlebars';
 
-    var path = req.url.toLowerCase();
-    switch(path) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('This is the Home Page!');
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('This is the About Page! My name is Arthur Tabadero and I am a student for IT122: Javascript 2. I am taking this class to learn more about JS and to learn the ways of Node.js!');
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Page not found!');
-            break;
-    }
+const app = express();
 
-}).listen(process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
+app.use(express.static('./public'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.engine('hbs', handlebars({defaultLayout: "main.hbs"}));
+app.set("view engine", "hbs");
+
+// Render default home page
+app.get('/', (req, res) => {
+    res.render('home', {pokemons: pokemon.getAll()});
+});
+
+// Render About page
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('This is the About Page! My name is Arthur Tabadero and I am a student for IT122: Javascript 2. I am taking this class to learn more about JS and to learn the ways of Node.js!');
+});
+
+app.get('/details', (req, res) => {
+    let result = pokemon.getItem(req.query.name);
+    let details = JSON.stringify(result);
+    res.render('details', {name: req.query.name, result: result, details: details});
+});
+
+// Render 404 handler
+app.use((req,res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 not found')
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started.');
+});
+
